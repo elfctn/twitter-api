9dcb8b4d42fd5794e8a0294ecf856e02c2322348
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-27 14:53:46 +0300

    note added

--- src/main/java/com/workintech/twitterapi/repository/UserRepository.java
+++ src/main/java/com/workintech/twitterapi/repository/UserRepository.java
@@ -8,3 +8,13 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
+
+
+//    /register endpointi için -----JpaRepository save(User user) hediye eder
+
+
+//    /login    endpointi----- yukarıdaki metoduu kullanır
+// optional var ya da yok döner içi boş optional kutusu
+//.isPresent(), .orElseThrow() gibi metotları kullanarız
+// bu durumu kontrol etmeye zorlar
+// NullPointerException riskini ortadan kaldırır
