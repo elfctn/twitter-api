36c3da68268aa6a34e74bbd431d59158ee14f99d
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-27 15:25:17 +0300

    RetweetRepository eklendi - repository katmanı tamamlandı

---
+++ src/main/java/com/workintech/twitterapi/repository/RetweetRepository.java
@@ -0,0 +1,22 @@
+package com.workintech.twitterapi.repository;
+
+import com.workintech.twitterapi.entity.Retweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RetweetRepository extends JpaRepository<Retweet, Long> {
+
+    boolean existsByTweetIdAndUserId(Long tweetId, Long userId);
+
+    Optional<Retweet> findByTweetIdAndUserId(Long tweetId, Long userId);
+
+
+}
+
+//"Retweetle" işlemi öncesinde, kullanıcının aynı tweeti
+// daha önce retweetleyip retweetlemediğini kontrol
+
+//Retweeti geri almak işlemi için
+// silinecek olan o spesifik Retweet kaydını
+// veritabanından bulmamı sağlayacak
