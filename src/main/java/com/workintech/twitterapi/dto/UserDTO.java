cc628fb46e89068ae8457ea93c0d8514fd528b2a
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-27 17:24:11 +0300

    sorunları ortadan kaldırmak için önden bir userDTO sınıfı yazıldı

---
+++ src/main/java/com/workintech/twitterapi/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.workintech.twitterapi.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    @NotNull
+    @Size(max = 50)
+    private String username;
+
+    @NotNull
+    @Size(max = 255)
+    private String password;
+
+    @NotNull
+    @Email
+    @Size(max = 100)
+    private String email;
+
+
+}
