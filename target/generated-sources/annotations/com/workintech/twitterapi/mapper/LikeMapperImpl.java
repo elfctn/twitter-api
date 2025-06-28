e2a10d98f81bea156875a788b361565f02adc9db
Elif Cetin <elifcetin.dev@gmail.com>
2025-06-28 17:12:41 +0300

    mapperimpl eklendi

--- target/generated-sources/annotations/com/workintech/twitterapi/mapper/LikeMapperImpl.java
+++ target/generated-sources/annotations/com/workintech/twitterapi/mapper/LikeMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-28T01:10:15+0300",
+    date = "2025-06-28T17:11:38+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
